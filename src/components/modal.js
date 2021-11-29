162d74d667ec9d9ad2d9052abf9da2f3954f400c
ninjacanthi1995 <hoangminhchaucp54@gmail.com>
2021-11-29 21:44:27 +0100

    Add modal component

---
+++ src/components/modal.js
@@ -0,0 +1,57 @@
+import '../App.scss';
+import React from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
+function ModalComponent() {
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    return (
+        <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+        >
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+                <input />
+                <button>tab navigation</button>
+                <button>stays</button>
+                <button>inside</button>
+                <button>the modal</button>
+            </form>
+        </Modal>
+    );
+}
+
+export default ModalComponent;
--- src/components/product-images.js
+++ src/components/product-images.js
@@ -1,19 +1,41 @@
 import '../App.scss';
 import {useState} from "react";
+import Modal from "react-modal";
 
 function ProductImages() {
     const [imgIdx, setImgIdx] = useState(1);
+    const [modalIsOpen, setIsOpen] = useState(false);
 
     const thumbnailImgs = [1, 2, 3, 4].map(elem => {
         return <img onClick={() => setImgIdx(elem)} key={elem} className="thumbnail" src={`./images/image-product-${elem}-thumbnail.jpg`} alt={`thumnail-${elem}`} />
     })
 
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
     return (
         <div id="product-images">
-            <img id="product-img" src={`./images/image-product-${imgIdx}.jpg`} alt="product" />
+            <img id="product-img" src={`./images/image-product-${imgIdx}.jpg`} alt="product" onClick={openModal} />
             <div id="thumbnails-container">
                 {thumbnailImgs}
             </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Example Modal"
+                className="Modal"
+                overlayClassName="Overlay"
+            >
+                <img src={`./images/image-product-${imgIdx}.jpg`} alt="product" />
+                <div>
+                    {thumbnailImgs}
+                </div>
+            </Modal>
         </div>
     );
 }
